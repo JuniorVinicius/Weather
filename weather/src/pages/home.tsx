76860d127417ddab80fa23aa063118f7165adb9c
Junior Vinicius <jr.vinicius.silva@gmail.com>
2022-01-09 13:12:19 -0300

    consultas na api

--- weather/src/pages/home.tsx
+++ weather/src/pages/home.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, {useState, useEffect}from 'react'
 import Background from '../components/background'
 import Conteiner from '../components/conteiner'
 import Header from '../components/header'
@@ -6,21 +6,61 @@ import { H1, H2, Span } from '../components/text/text'
 import Box from '../components/box'
 import imgSun from '../images/Sun.png'
 import imgCloudSun from '../images/CloudSun.png'
+import axios from 'axios'
 
+interface Local{
+  name: string,
+  weather:any,
+  main: any
+}
 export default function Home() {
+  
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState<Local>();
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    })
+  }, [])
+
+  let getWeather = async (lat: any, long: any)=>{
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params:{
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  } 
+
+
+
+if (location == false) {
+  
   return (
+    <>
+      Você precisa habilitar a localização no browser o/
+    </>
+  )
+}else{
+     return (
     <Background>
       <Header>
-        <H2>Ilha das Flores</H2>
+        <H2>{weather?.name}</H2>
       </Header>
 
       <Conteiner itemProp="column">
         <img src={imgSun} alt="" />
 
-        <H1>27°</H1>
+        <H1>{JSON.stringify(Math.floor(weather?.main.temp)) + "°"}</H1>
 
-        <p style={{ fontSize: 30 }}>Sun</p>
-        <p style={{ fontSize: 20 }}>RealFeel 30°</p>
+        <p style={{ fontSize: 30, textTransform: 'capitalize'}}>{weather?.weather[0].description}</p>
+        <p style={{ fontSize: 20 }}>RealFeel {JSON.stringify(Math.floor(weather?.main.feels_like)) + "°"}</p>
       </Conteiner>
 
       <Conteiner>
@@ -68,4 +108,7 @@ export default function Home() {
       </Conteiner>
     </Background>
   )
+  }
+
+ 
 }
